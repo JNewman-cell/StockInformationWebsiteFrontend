9a8c47f0a4bf0d8552e9c1f9233f77aff910dac8
Jackson Newman <jpnewman167@gmail.com>
2025-11-11 18:36:20 -0800

    Added deploy for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/StockInformationWebsiteFrontend/',
   plugins: [react()],
   server: {
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080',
         changeOrigin: true,
         secure: false,
       },
