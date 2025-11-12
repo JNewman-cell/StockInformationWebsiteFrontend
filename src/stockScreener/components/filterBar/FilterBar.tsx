ef3898cd53d3c8cc305b30793b304b481b8ce924
Jackson Newman <jpnewman167@gmail.com>
2025-11-12 13:55:12 -0800

    Added filter icon back using svg instead

--- src/stockScreener/components/filterBar/FilterBar.tsx
+++ src/stockScreener/components/filterBar/FilterBar.tsx
@@ -307,7 +307,9 @@ const Filters: FC<FiltersProps> = ({ onFilterChange }) => {
           )}
           {appliedCount > 0 && (
             <span className="filters-badge action-badge">
-              <i className="bi bi-funnel-fill"></i>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-funnel-fill" viewBox="0 0 16 16">
+                <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5z"/>
+              </svg>
               <span className="badge-count">{appliedCount}</span>
             </span>
           )}
